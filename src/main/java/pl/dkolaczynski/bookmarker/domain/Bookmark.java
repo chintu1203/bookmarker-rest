9ac1e0bd71335ce3e8ea89fd0da6ae5ae50acacf
dklaczynski <dklaczynski@gmail.com>
2014-08-25 21:00:00 +0200

    added JPA support

--- src/main/java/pl/dkolaczynski/bookmarker/domain/Bookmark.java
+++ src/main/java/pl/dkolaczynski/bookmarker/domain/Bookmark.java
@@ -2,13 +2,26 @@ package pl.dkolaczynski.bookmarker.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
+@Entity
+@Table(name = "bookmarks")
 @SuppressWarnings("serial")
 public class Bookmark implements Serializable {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@Column(nullable = false)
 	private String name;
 	private String url;
 	private String description;
